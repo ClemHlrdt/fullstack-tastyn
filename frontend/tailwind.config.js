6987e322a1f9d5ad1d3b8d612ebeccb576f56d1d
Clément Hélardot <clement.helardot@gmail.com>
2020-12-16 12:29:27 +0100

    removed unused config entries

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -43,10 +43,6 @@ module.exports = {
       minHeight: theme => ({
         "90": "90vh",
         "95": "95vh",
-        "screen/2": "50vh",
-        "screen/3": "calc(100vh / 3)",
-        "screen/4": "calc(100vh / 4)",
-        "screen/5": "calc(100vh / 5)",
       }),
     },
     fontFamily: {
