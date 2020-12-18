60919834627229aab0bb8f137c08370aee537369
Clément Hélardot <clement.helardot@gmail.com>
2020-12-18 09:45:39 +0100

    Added fadein animation for the recipe list

--- frontend/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -5,10 +5,36 @@ import { RecipePage } from 'src/app/models/recipe-page.model';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipesService } from 'src/app/services/recipes/recipes.service';
 
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        stagger('200ms', animate('600ms ease-in-out', style({ opacity: 1 }))),
+      ],
+      { optional: true }
+    ),
+    query(':leave', animate('200ms', style({ opacity: 0 })), {
+      optional: true,
+    }),
+  ]),
+]);
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss'],
+  animations: [listAnimation],
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
