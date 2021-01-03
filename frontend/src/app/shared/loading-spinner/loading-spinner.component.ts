22bdff6d4cf3a4a52b71dba7326c7b7b074d536d
Clément Hélardot <clement.helardot@gmail.com>
2021-01-03 16:08:27 +0100

    Sorted css classes using headwind extension

--- frontend/src/app/shared/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading-spinner',
   template:
-    '<div class="container h-95 mx-auto"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>',
+    '<div class="container mx-auto h-95"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>',
   styleUrls: ['./loading-spinner.component.scss'],
 })
 export class LoadingSpinnerComponent {}
